61717a80174dad8d2958a5cee83ed833621821db
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-05-28 13:32:38 +0200

    Created Basic Views

--- roomquery/roomquery/core/views.py
+++ roomquery/roomquery/core/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from . models import Property
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def login_view(request):
+    return render(request, 'core/login.html')
+
+def register(request):
+    return render(request, 'core/register.html')
+
+class PropertyListView(ListView):
+    model = Property
+    template_name = 'core/property_list.html'
+
+class PropertyDetailView(DetailView):
+    model = Property
+    template_name = 'core/property_detail.html'
