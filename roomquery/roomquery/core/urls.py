d98b5f5fc6ff73a63c06df5dae2b307e7afa5a2e
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-05-29 02:47:19 +0200

    created user authentication

--- roomquery/roomquery/core/urls.py
+++ roomquery/roomquery/core/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+from .views import signup
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login_view, name='login'),
+    #path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
     path('properties/', views.PropertyListView.as_view(), name='property_list'),
     path('property/<int:pk>/', views.PropertyDetailView.as_view(), name='property_detail'),
+    path('signup/', signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout'),
 ]
--- roomquery/roomquery/core/views.py
+++ roomquery/roomquery/core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from . models import Property
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
@@ -20,3 +22,14 @@ class PropertyListView(ListView):
 class PropertyDetailView(DetailView):
     model = Property
     template_name = 'core/property_detail.html'
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to a desired page after sign-up
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
